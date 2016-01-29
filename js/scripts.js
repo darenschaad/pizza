b7caccb99e8431b0521ed402c00d7d750ee4b45c
Daren Schaad <darenschaad@gmail.com>
2016-01-29 15:06:55 -0800

    Add ability for user to look at the details of all of the pizzas they have constructed

--- js/scripts.js
+++ js/scripts.js
@@ -16,110 +16,38 @@ Pizza.prototype.price = function() {
   }
 }
 
-
-
 $(document).ready(function(){
 
 
   $("form#new-pizza").submit(function(event){
     event.preventDefault();
-    debugger;
     var pizzaSize = $("#pizzaSize").val();
     var newPizza = new Pizza (pizzaSize);
     var meatToppings = $("input:checkbox:checked.meat").map(function() {
       newPizza.meatToppings.push((this).value);
     })
+
     var vegToppings = $("input:checkbox:checked.veg").map(function() {
       newPizza.vegToppings.push((this).value);
     })
-    $("ul#pizzas").append("<li><span class ='contact'>" + newPizza.size + " Pizza - " + "$" + newPizza.price() + "</span></li>");
+
+    $("ul#pizzas").append("<li><span class ='pizza'>" + newPizza.size + " Pizza - " + "$" + newPizza.price() + "</span></li>");
     $(".checkout").show();
+
+    var totalCost
+    $("h4#cost").append()
+
+    $(".pizza").last().click(function() {
+      $("#show-pizza").show();
+      $("#show-pizza h2").text(newPizza.size);
+      $(".meatToppings").text(newPizza.meatToppings);
+      $(".vegToppings").text(newPizza.vegToppings);
+      $(".price").text(newPizza.price());
+    })
+
+
+
   })
 
 
 })
-// return this.value;
-// $("#meat input:checkbox:checked").each(function(){
-//
-// function Contact(firstName, lastName) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.addresses = [];
-// };
-//
-// Contact.prototype.fullName = function() {
-//   return this.firstName + " " + this.lastName;
-// };
-//
-// function Address(addressType, street, city, state) {
-//   this.addressType = addressType;
-//   this.street = street;
-//   this.city = city;
-//   this.state = state;
-// };
-//
-// Address.prototype.fullAddress = function() {
-//   return this.addressType + ", " + this.street + ", " + this.city + ", " + this.state;
-// };
-//
-// $(document).ready(function() {
-//   $("#add-address").click(function() {
-//     $("#new-addresses").append('<div class="new-address">' +
-//                                  '<div class="form-group">' +
-//                                    '<lable for="new-type">Address Type</label>' +
-//                                    '<input type="text" class="form-control new-type">'+
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-street">Street</label>' +
-//                                    '<input type="text" class="form-control new-street">' +
-//                                  '</div>' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-city">City</label>' +
-//                                    '<input type="text" class="form-control new-city">' +
-//                                  '</div>' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-state">State</label>' +
-//                                    '<input type="text" class="form-control new-state">' +
-//                                  '</div>' +
-//                                '</div>');
-//   });
-//
-//   $("form#new-contact").submit(function(event) {
-//     event.preventDefault();
-//
-//     var inputtedFirstName = $("input#inputtedFirstName").val();
-//     var inputtedLastName = $("input#inputtedLastName").val();
-//     var newContact = new Contact (inputtedFirstName, inputtedLastName);
-// //
-//
-//     $(".new-address").each(function() {
-//       var inputtedAddressType = $(this).find("input.new-type").val();
-//       var inputtedStreet = $(this).find("input.new-street").val();
-//       var inputtedCity = $(this).find("input.new-city").val();
-//       var inputtedState = $(this).find("input.new-state").val();
-//       var newAddress = new Address (inputtedAddressType, inputtedStreet, inputtedCity, inputtedState);
-//       newContact.addresses.push(newAddress);
-//     });
-//
-//     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-//
-//     $(".contact").last().click(function() {
-//       $("#show-contact").show();
-//       $("#show-contact h2").text(newContact.fullName());
-//       $(".first-name").text(newContact.firstName);
-//       $(".last-name").text(newContact.lastName);
-//       $("ul#addresses").text("");
-//       newContact.addresses.forEach(function(address) {
-//         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-//       });
-//     });
-//
-//     // debugger;
-//     $("input#inputtedFirstName").val("");
-//     $("input#inputtedLastName").val("");
-//     $("input.new-type").val("");
-//     $("input.new-street").val("");
-//     $("input.new-city").val("");
-//     $("input.new-state").val("");
-//
-//   });
-// });
