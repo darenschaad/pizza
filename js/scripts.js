fcafd9725931cc1cc7db6514186da0f3431032a7
Daren Schaad <darenschaad@gmail.com>
2016-01-29 08:37:50 -0800

    pass first spec, creating pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,89 @@
-function Contact(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.addresses = [];
-};
+function Pizza(size){
+  this.size = size;
+  this.toppings = [];
+}
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-};
 
-function Address(addressType, street, city, state) {
-  this.addressType = addressType;
-  this.street = street;
-  this.city = city;
-  this.state = state;
-};
 
-Address.prototype.fullAddress = function() {
-  return this.addressType + ", " + this.street + ", " + this.city + ", " + this.state;
-};
-
-$(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<lable for="new-type">Address Type</label>' +
-                                   '<input type="text" class="form-control new-type">'+
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
-  });
-
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#inputtedFirstName").val();
-    var inputtedLastName = $("input#inputtedLastName").val();
-    var newContact = new Contact (inputtedFirstName, inputtedLastName);
+// function Contact(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.addresses = [];
+// };
 //
-
-    $(".new-address").each(function() {
-      var inputtedAddressType = $(this).find("input.new-type").val();
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address (inputtedAddressType, inputtedStreet, inputtedCity, inputtedState);
-      newContact.addresses.push(newAddress);
-    });
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-
-    // debugger;
-    $("input#inputtedFirstName").val("");
-    $("input#inputtedLastName").val("");
-    $("input.new-type").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-
-  });
-});
+// Contact.prototype.fullName = function() {
+//   return this.firstName + " " + this.lastName;
+// };
+//
+// function Address(addressType, street, city, state) {
+//   this.addressType = addressType;
+//   this.street = street;
+//   this.city = city;
+//   this.state = state;
+// };
+//
+// Address.prototype.fullAddress = function() {
+//   return this.addressType + ", " + this.street + ", " + this.city + ", " + this.state;
+// };
+//
+// $(document).ready(function() {
+//   $("#add-address").click(function() {
+//     $("#new-addresses").append('<div class="new-address">' +
+//                                  '<div class="form-group">' +
+//                                    '<lable for="new-type">Address Type</label>' +
+//                                    '<input type="text" class="form-control new-type">'+
+//                                  '<div class="form-group">' +
+//                                    '<label for="new-street">Street</label>' +
+//                                    '<input type="text" class="form-control new-street">' +
+//                                  '</div>' +
+//                                  '<div class="form-group">' +
+//                                    '<label for="new-city">City</label>' +
+//                                    '<input type="text" class="form-control new-city">' +
+//                                  '</div>' +
+//                                  '<div class="form-group">' +
+//                                    '<label for="new-state">State</label>' +
+//                                    '<input type="text" class="form-control new-state">' +
+//                                  '</div>' +
+//                                '</div>');
+//   });
+//
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//
+//     var inputtedFirstName = $("input#inputtedFirstName").val();
+//     var inputtedLastName = $("input#inputtedLastName").val();
+//     var newContact = new Contact (inputtedFirstName, inputtedLastName);
+// //
+//
+//     $(".new-address").each(function() {
+//       var inputtedAddressType = $(this).find("input.new-type").val();
+//       var inputtedStreet = $(this).find("input.new-street").val();
+//       var inputtedCity = $(this).find("input.new-city").val();
+//       var inputtedState = $(this).find("input.new-state").val();
+//       var newAddress = new Address (inputtedAddressType, inputtedStreet, inputtedCity, inputtedState);
+//       newContact.addresses.push(newAddress);
+//     });
+//
+//     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+//
+//     $(".contact").last().click(function() {
+//       $("#show-contact").show();
+//       $("#show-contact h2").text(newContact.fullName());
+//       $(".first-name").text(newContact.firstName);
+//       $(".last-name").text(newContact.lastName);
+//       $("ul#addresses").text("");
+//       newContact.addresses.forEach(function(address) {
+//         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+//       });
+//     });
+//
+//     // debugger;
+//     $("input#inputtedFirstName").val("");
+//     $("input#inputtedLastName").val("");
+//     $("input.new-type").val("");
+//     $("input.new-street").val("");
+//     $("input.new-city").val("");
+//     $("input.new-state").val("");
+//
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,28 +1,7 @@
-describe ('Contact', function(){
-  it("creates a new contact with the given properties", function() {
-    var newContact = new Contact("Daren", "Schaad");
-    expect(newContact.firstName).to.equal("Daren");
-    expect(newContact.lastName).to.equal("Schaad");
-    expect(newContact.addresses).to.eql([]);
+describe ('Pizza', function(){
+  it("Creates a new pizza with a size property, and a toppings property", function() {
+    var testPizza = new Pizza("Large");
+    expect(testPizza.size).to.equal("Large");
+    expect(testPizza.toppings).to.eql([]);
   });
-
-  it("adds the fullName method to a contact", function () {
-    var newContact = new Contact("Taylor", "Pokoj");
-    expect(newContact.fullName()).to.equal("Taylor Pokoj");
-  });
-
-});
-
-describe('Address', function() {
-  it("creates a new address with the given specifications", function() {
-    var testAddress = new Address("Home", "123 Main St", "Test City","Test State");
-    expect(testAddress.addressType).to.equal("Home")
-    expect(testAddress.street).to.equal("123 Main St");
-    expect(testAddress.city).to.equal("Test City");
-    expect(testAddress.state).to.equal("Test State");
-  });
-  it("adds the fullAddress method to all addresses", function(){
-    var testAddress = new Address("Home", "400 SW 6th St.", "Portland", "OR");
-    expect (testAddress.fullAddress()).to.equal("Home, 400 SW 6th St., Portland, OR");
-  })
 });
